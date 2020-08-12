1ba17826da57171e363948a0e8e334e31950acd7
Rob Chapman <autosexual@gmail.com>
2020-08-12 15:27:50 +0900

    added user geocode and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :user_books
   has_many :books, through: :user_books
+  # validates :address, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
