6f8b5b26f20c8b844e8cc80e5915923390cc4089
Moe Aoki <mt215tjk@gmail.com>
2020-08-13 01:23:56 +0900

    Added bookshelf method in user_books controller. Authorized bookshelf? in policy. Created view for bookshelf

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -2,12 +2,20 @@ class UserBooksController < ApplicationController
   def index
     @user_books = policy_scope(UserBook)
   end
+  # one user's bookshelf
+  def bookshelf
+    @user_book = policy_scope(UserBook).find(params[:id])
+    @user = @user_book.user
+    @user_books = policy_scope(UserBook).where(user: @user)
+    authorize @user_book
+  end
 
   def show
     @user_book = policy_scope(UserBook).find(params[:id])
     @user = @user_book.user
     authorize @user_book
   end
+
 
   # def new
   # end
