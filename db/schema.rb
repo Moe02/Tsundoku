2e655ee689992b713343068a9155a61ebdbca654
Moe Aoki <mt215tjk@gmail.com>
2020-08-13 12:13:39 +0900

    Request model/controller/view index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_072504) do
+ActiveRecord::Schema.define(version: 2020_08_12_113249) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 2020_08_12_072504) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "cover"
     t.string "keywords", default: [], array: true
+  end
+
+  create_table "requests", force: :cascade do |t|
+    t.string "status"
+    t.date "start_date"
+    t.date "end_date"
+    t.bigint "user_id", null: false
+    t.bigint "user_book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_book_id"], name: "index_requests_on_user_book_id"
+    t.index ["user_id"], name: "index_requests_on_user_id"
   end
 
   create_table "user_books", force: :cascade do |t|
@@ -49,6 +61,8 @@ ActiveRecord::Schema.define(version: 2020_08_12_072504) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "requests", "user_books"
+  add_foreign_key "requests", "users"
   add_foreign_key "user_books", "books"
   add_foreign_key "user_books", "users"
 end
