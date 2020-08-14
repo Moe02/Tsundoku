1801a348c16daf89f71d19724a4964700080e5bd
Rob Chapman <autosexual@gmail.com>
2020-08-14 15:43:59 +0900

    seeds increased quite a lot

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 require 'open-uri'
 require 'json'
 
-isbns = ['0451526538', '2207109089', '0307386457', '0002008408', '0755392620', '0375420525', '0099528487']
+isbns = ['0451526538', '2207109089', '0307386457', '0002008408', '0755392620', '0375420525', '0099528487', '9781784871055', '0765379295', '0393334155', '0393355942', '0062119044', ' 0679735771', '0393341763', '0812988523', '9780451526342', '0399501487', '9780140424393', '0441569595', '0553380958', '0345404475', '0679728759', '1583226982', '1779501129', '034549752X', '0553283685', '0140232923', '0375726543', '0679733787', '0679761047', '0679775439', '0765335301', '0441627404', '054792822X', '0575086254']
 
 puts "Cleaning requests..."
 Request.destroy_all
@@ -28,6 +28,11 @@ isbns.each do |isbn|
   next unless book_hash
   book_hash["cover"] ? cover = book_hash["cover"]["large"] : cover = ""
   keywords = []
+  if book_hash["authors"].class == Array
+    author = book_hash["authors"].first["name"]
+  else
+    author = ""
+  end
   if book_hash["subjects"]
     book_hash["subjects"].each do |hash|
       keywords << hash["name"]
@@ -37,7 +42,7 @@ isbns.each do |isbn|
   next unless cover.present?
     Book.create!(
     title: book_hash["title"],
-    author: book_hash["authors"].first["name"],
+    author: author,
     cover: cover,
     keywords: keywords
     )
@@ -46,6 +51,13 @@ end
 puts "#{Book.count} books created."
 
 puts "Creating new users..."
+20.times do
+  User.create!(
+    email: Faker::Internet.email,
+    password: "password",
+    username: Faker::Name.first_name
+  )
+end
 
 User.create!(
     email: "rob@caramail.com",
@@ -64,23 +76,14 @@ User.create!(
     password: "password",
     username: "moe"
   )
-
-
-5.times do
-  User.create!(
-    email: Faker::Internet.email,
-    password: "password",
-    username: Faker::Name.first_name
-  )
-end
-
 puts "#{User.count} Users created."
+
 
 puts "Creating example user books..."
 users = User.all
 books = Book.all
 users.each do |user|
-  rand(1..4).times do
+  rand(5..10).times do
     UserBook.create!(
       user: user,
       book: books.sample
@@ -88,6 +91,7 @@ users.each do |user|
   end
 end
 puts "#{UserBook.count} user books created."
+
 
 puts "Generating requests"
 userbooks = UserBook.all
