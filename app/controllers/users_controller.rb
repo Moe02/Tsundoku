6e394105aa93e972438db25f69950700b1610019
Rob Chapman <autosexual@gmail.com>
2020-08-14 14:22:54 +0900

    other tabs operational

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   def dashboard
     skip_authorization
     # @requests_received = Request.where(user_book: current_user.user_books)
-    @requests_received = current_user.requests_received
-    @requests_made = current_user.requests
-    @requests_active = ["active", "active", "active"]
+    @requests_received = current_user.requests_received.where(status: "pending")
+    @requests_made = current_user.requests.where(status: "pending")
+    @borrowed = current_user.requests.where(status: "active")
+    @loaned = current_user.requests_received.where(status: "active")
+    @history = Request.where(status: "history")
   end
 end
