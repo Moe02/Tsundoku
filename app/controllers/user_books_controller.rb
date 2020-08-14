222716a0b64cc1c2514ab8f5850dd078a8e693de
veronica puspita <caslavia@gmail.com>
2020-08-14 14:11:34 +0900

    improve the bookshelf display

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -9,7 +9,7 @@ class UserBooksController < ApplicationController
   end
   # one user's bookshelf
   def bookshelf
-    @user_book = policy_scope(UserBook).find(params[:id])
+    @user_book = UserBook.find(params[:id])
     @user = @user_book.user
     @user_books = policy_scope(UserBook).where(user: @user)
     authorize @user_book
